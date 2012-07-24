0e191685825406b8f4cf0a3e5cf05064ddb92d73
Chris <Chris@lipa>
2012-07-24 11:27:24 -0700

    aaa

--- build/bot.build/Debug/bot.build/Objects-normal/x86_64/BattleDocumentViewController.d
+++ build/bot.build/Debug/bot.build/Objects-normal/x86_64/BattleDocumentViewController.d
@@ -1,3 +1,4 @@
 dependencies: /Users/lipa/bot/bot/BattleDocumentViewController.m \
   /Users/lipa/bot/bot/BattleDocumentViewController.h \
+  /Users/lipa/bot/bot/RobotListTableView.h \
   /Users/lipa/bot/bot/BattleDocument.h
