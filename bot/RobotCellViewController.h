95233cd1f9be3e7f80350805ba1936e7c0f6b224
Chris <Chris@lipa>
2012-07-29 16:03:06 -0700

    Hooked up robot list

---
+++ bot/RobotCellViewController.h
@@ -0,0 +1,13 @@
+//
+//  RobotCellViewController.h
+//  bot
+//
+//  Created by Christopher Lipa on 7/29/12.
+//
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface RobotCellViewController : NSViewController
+
+@end
---
+++ bot/RobotCellViewController.m
@@ -0,0 +1,27 @@
+//
+//  RobotCellViewController.m
+//  bot
+//
+//  Created by Christopher Lipa on 7/29/12.
+//
+//
+
+#import "RobotCellViewController.h"
+
+@interface RobotCellViewController ()
+
+@end
+
+@implementation RobotCellViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Initialization code here.
+    }
+    
+    return self;
+}
+
+@end
