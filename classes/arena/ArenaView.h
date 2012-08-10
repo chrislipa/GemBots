7651ba0728aa8ab70c09750ea3b613812b9acafb
Chris <Chris@lipa>
2012-08-09 21:44:40 -0700

    TIming.

--- classes/arena/ArenaView.h
+++ classes/arena/ArenaView.h
@@ -9,10 +9,13 @@
 #import <Cocoa/Cocoa.h>
 #import "ArenaViewController.h"
 #import "GameStateDescriptor.h"
+#import "BattleDocumentViewController.h"
 @class ArenaViewController;
 @interface ArenaView : NSOpenGLView {
+    IBOutlet BattleDocumentViewController* battleDocumentViewController;
     IBOutlet ArenaViewController* arenaViewController;
     NSObject<GameStateDescriptor>* gameStateDescriptor;
+    double lastTimeABufferSwapWasPerformed;
 }
 
 @property (readwrite,retain)  NSObject<GameStateDescriptor>* gameStateDescriptor;
