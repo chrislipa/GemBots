326088770e12d810571aea34cf31a5db9dd753c7
Chris <Chris@lipa>
2012-08-18 18:12:50 -0700

    Audio tweaks

--- classes/battledocument/AudioController.m
+++ classes/battledocument/AudioController.m
@@ -29,14 +29,60 @@ static void callback(void *a, AudioQueueRef b, AudioQueueBufferRef c)
 }
 
 
+static AudioQueueBufferRef buffer[3];
+static AudioStreamBasicDescription dataFormat;
+static AudioStreamPacketDescription *audioStreamPacketDescription[3] = {nil,nil,nil};
+static UInt32 s[3];
+
+void initBuffer(NSString* name, AudioQueueBufferRef b[3]) {
+    NSURL* url = [[NSBundle mainBundle] URLForResource:name withExtension:@"caf"];
+    CFURLRef theURL = (__bridge CFURLRef)(url);
+    AudioFileID audioFileID;
+    AudioFileOpenURL(theURL, 0x01, kAudioFileCAFType, &audioFileID);
+    CFRelease(theURL);
+    
+	
+    UInt32 size = sizeof(dataFormat);
+    AudioFileGetProperty(audioFileID, kAudioFilePropertyDataFormat, &size, &dataFormat);
+    AudioQueueRef audioQueue;
+	AudioQueueNewOutput(&dataFormat, callback, nil, nil, nil, 0, &audioQueue);
+    UInt32 packs = 0x20000 / dataFormat.mBytesPerPacket;
+   
+	
+    
+    UInt32 ind = 0,n;
+    for (int i = 0; i < 3; i++) {
+		AudioQueueAllocateBuffer(audioQueue, 0x20000, &b[i]);
+        AudioQueueBufferRef bu = b[i];
+        s[i] = packs;
+        AudioFileReadPackets(audioFileID, NO, &n, audioStreamPacketDescription[i], ind, &s[i], bu->mAudioData);
+        ind += s[i];
+        
+	}
+}
 
 
 
 
-
+void playFromBuffer(AudioQueueBufferRef c[3]) {
+    AudioQueueRef audioQueue;
+    AudioQueueNewOutput(&dataFormat, callback, nil, nil, nil, 0, &audioQueue);
+    AudioQueueBufferRef b[3];
+//    AudioQueueSetParameter(audioQueue, kAudioQueueParam_Volume, 0.1);
+    UInt32 ind = 0;
+    for (int i = 0; i < 3; i++) {
+       
+		
+        ind += s[i];
+        AudioQueueEnqueueBuffer(audioQueue, b[i], (audioStreamPacketDescription[i] ? s[i] : 0), audioStreamPacketDescription[i]);
+    }
+    
+    
+}
 
 
 void playSoundEffect(NSString* name, float volume) {
+    AudioQueueRef audioQueue;
     NSURL* url = [[NSBundle mainBundle] URLForResource:name withExtension:@"caf"];
     CFURLRef theURL = (__bridge CFURLRef)(url);
     AudioFileID audioFileID;
@@ -46,7 +92,7 @@ void playSoundEffect(NSString* name, float volume) {
 	AudioStreamBasicDescription dataFormat;
     UInt32 size = sizeof(dataFormat);
     AudioFileGetProperty(audioFileID, kAudioFilePropertyDataFormat, &size, &dataFormat);
-    AudioQueueRef audioQueue;
+
 	AudioQueueNewOutput(&dataFormat, callback, nil, nil, nil, 0, &audioQueue);
     UInt32 packs = 0x20000 / dataFormat.mBytesPerPacket;
     AudioStreamPacketDescription *audioStreamPacketDescription = nil;
@@ -64,9 +110,10 @@ void playSoundEffect(NSString* name, float volume) {
             AudioQueueEnqueueBuffer(audioQueue, bu, (audioStreamPacketDescription ? s : 0), audioStreamPacketDescription);
         }
 	}
-	AudioQueueSetParameter(audioQueue, kAudioQueueParam_Volume, volume);
-    AudioQueueStart(audioQueue, nil);
-    //return queue;
+    AudioQueueSetParameter(audioQueue, kAudioQueueParam_Volume, volume);
+	AudioQueueStart(audioQueue, nil);
+    
+    
 }
 
 void playQueue(AudioQueueRef queue) {
@@ -78,7 +125,7 @@ void playQueue(AudioQueueRef queue) {
 -(id) init {
     if (self = [super init]) {
         laserSoundEffects = [NSMutableArray array];
-        
+        initBuffer(@"laser3-lipa-modified", buffer);
         for (int i = 0; i < 200; i++) {
             //AudioQueueRef ptr = loadSoundEffect(@"laser3-lipa-modified");
             //[laserSoundEffects addObject:[NSValue valueWithPointer:ptr ]];
@@ -94,8 +141,13 @@ void playQueue(AudioQueueRef queue) {
 }
 
 -(void) playMissileFireSound:(int) times :(double) timePeriod {
-    for (int i = 0; i < times; i++) {
-        playSoundEffect(@"laser3-lipa-modified",0.1);
+    //playFromBuffer(buffer);
+    
+    if (times >= 1) {
+        
+    
+    
+        playSoundEffect(@"laser3-lipa-modified",0.08);
         /*
         currentMissileFire = (currentMissileFire + 1) % [laserSoundEffects count];
         int old = (currentMissileFire + [laserSoundEffects count]/2) % [laserSoundEffects count];
@@ -104,26 +156,17 @@ void playQueue(AudioQueueRef queue) {
         playQueue(ptr);
         AudioQueueReset(oldptr);*/
         
+    
     }
 }
 -(void) playMissileExplodeSound:(int) times :(double) timePeriod {
     
    
-    for (int i = 0; i < times; i++) {
-        
-        /*
-        AVAudioPlayer* a = [explosionSoundEffects objectAtIndex:currentExplosion];
-        if ([a isPlaying]) {
-            break;
-        }
-        currentExplosion = (currentExplosion + 1) % [explosionSoundEffects count];
-        double delay;
-        delay = 0+ i * timePeriod / times;
-        //delay = .10*i;
-        [a playAtTime:a.deviceCurrentTime+delay];*/
-        
+    if (times >= 1) {
         playSoundEffect(@"21410_21830-lq-short-lipa",0.8);
     }
+    
+
 }
 
 @end
