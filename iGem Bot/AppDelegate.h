38fb031c39d3e125c6eece4d475491d16822bcf4
Chris <Chris@lipa>
2012-08-31 10:23:16 -0700

    igembot

---
+++ iGem Bot/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  iGem Bot
+//
+//  Created by Christopher Lipa on 8/30/12.
+//  Copyright (c) 2012 Christopher Lipa. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
---
+++ iGem Bot/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  iGem Bot
+//
+//  Created by Christopher Lipa on 8/30/12.
+//  Copyright (c) 2012 Christopher Lipa. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
