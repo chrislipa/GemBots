2a65851e0f9cf204cfde8c38b3688813178cfa5c
jalex27 <jalexander@pocketgems.com>
2012-10-03 08:23:27 -0700

    Make git commit more resilient to git not being present.

--- scripts/gitcommit.py
+++ scripts/gitcommit.py
@@ -1,7 +1,35 @@
 import os
 import subprocess
 
-x = subprocess.check_output("git log -1",shell=True)
+
+def which(program):
+
+    def is_exe(fpath):
+        return os.path.isfile(fpath) and os.access(fpath, os.X_OK)
+    
+    fpath, fname = os.path.split(program)
+    if fpath:
+        if is_exe(program):
+            return program
+    else:
+        for path in os.environ["PATH"].split(os.pathsep):
+            exe_file = os.path.join(path, program)
+            if is_exe(exe_file):
+                return exe_file
+    
+    return None
+
+
+
+check_git = which('git')
+if check_git == None:
+  x = 'a none'
+else:
+  try:
+    x = subprocess.check_output("git log -1",shell=True)
+  except:
+    x = 'a none'
+  
 
 sha = x.split("\n")[0].split(" ")[1]
 
