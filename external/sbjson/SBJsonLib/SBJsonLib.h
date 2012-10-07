fc822646ce7ec67941b54f2d171d1b940ea54b0c
Chris <Chris@lipa>
2012-10-06 22:41:50 -0700

    include sbjson as a non-submodule

---
+++ external/sbjson/SBJsonLib/SBJsonLib.h
@@ -0,0 +1,13 @@
+//
+//  SBJsonLib.h
+//  SBJsonLib
+//
+//  Created by Christopher Lipa on 8/17/12.
+//  Copyright (c) 2012 Stig Brautaset. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface SBJsonLib : NSObject
+
+@end
---
+++ external/sbjson/SBJsonLib/SBJsonLib.m
@@ -0,0 +1,13 @@
+//
+//  SBJsonLib.m
+//  SBJsonLib
+//
+//  Created by Christopher Lipa on 8/17/12.
+//  Copyright (c) 2012 Stig Brautaset. All rights reserved.
+//
+
+#import "SBJsonLib.h"
+
+@implementation SBJsonLib
+
+@end
